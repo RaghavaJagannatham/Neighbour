7d0e26c26ee957d5a5748da9ff1f07dea67d1bfc
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2025-01-19 18:22:15 +0530

    major changes added clerk in it

--- app/utils/fetchUserData.ts
+++
@@ -1,9 +0,0 @@
-// app/utils/fetchUserData.ts
-
-export async function fetchUserData(userId: string) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/profile/${userId}`);
-  if (!response.ok) {
-    return null;
-  }
-  return response.json();
-}
